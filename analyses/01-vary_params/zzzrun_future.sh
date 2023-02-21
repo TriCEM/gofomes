360c0ff2bdd7eecd2c073331042766c0881e7a1e
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-02-21 12:11:41 -0500

    update future

--- analyses/01-vary_params/zzzrun_future.sh
+++ analyses/01-vary_params/zzzrun_future.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=fomes_sim_networks
+#SBATCH --job-name=fomes_sim_varyparams
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=nbrazeau@med.unc.edu
 #SBATCH --ntasks=36
 #SBATCH --mem=64G
 #SBATCH --time=5-00:00:00
-#SBATCH --output=fomes_networks_%j.log
+#SBATCH --output=fomes_varyparams_%j.log
 
 R CMD BATCH 01-run_fomes_on_maestro.R
-
